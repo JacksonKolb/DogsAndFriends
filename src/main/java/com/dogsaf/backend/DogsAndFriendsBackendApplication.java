1a8d87bdfceceb74d1c0e420ff1c737f36182658
Jack Kolb <JacksonKolb94@gmail.com>
2022-03-14 14:30:11 -0600

    Spring application working

---
+++ src/main/java/com/dogsaf/backend/DogsAndFriendsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dogsaf.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsAndFriendsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogsAndFriendsBackendApplication.class, args);
+    }
+
+}
